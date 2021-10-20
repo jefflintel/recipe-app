3de5b25fc90a0202cc491e59d5739bc3942a286b
Jeff Lintel <jefflintel@gmail.com>
2021-10-20 08:50:25 -0500

    updated recipe selection process

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeHasBeenSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test Soup', 'Pretend soup, for testing purposes', 'https://placekitten.com/200/200'),
     new Recipe('Test Omlette', 'Pretend omlette, for testing purposes', 'https://placekitten.com/250/250')
@@ -17,4 +18,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeHasBeenSelected.emit(recipe);
+  }
 }
